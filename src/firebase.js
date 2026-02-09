77635e789f7a4778dd67fe1db34567d616af6a7b
mcisneros0046-pixel <mcisneros0046@icloud.com>
2026-02-09 13:12:46 -0800

    Add OAuthProvider for Apple authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,23 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyDVBpC-BPFNKyt9xU9lZEcZU1L7z1buMrk",
-  authDomain:        "star-flow-796c6.firebaseapp.com",
-  projectId:         "star-flow-796c6",
-  storageBucket:     "star-flow-796c6.firebasestorage.app",
+  apiKey: "AIzaSyDVBpC-BPFNKyt9xU9lZEcZU1L7z1buMrk",
+  authDomain: "star-flow-796c6.firebaseapp.com",
+  projectId: "star-flow-796c6",
+  storageBucket: "star-flow-796c6.firebasestorage.app",
   messagingSenderId: "53315400000",
-  appId:             "1:53315400000:web:2c1065187bb65393d80aa5",
+  appId: "1:53315400000:web:2c1065187bb65393d80aa5",
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const appleProvider = new OAuthProvider("apple.com");
+appleProvider.addScope("email");
+appleProvider.addScope("name");
+
+export { auth, googleProvider, appleProvider, db };
